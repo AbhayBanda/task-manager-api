e513cf597ca7a52e14bb0c1e88255be2b0db6801
Abhay Banda <bandaabhay@gmail.com>
2023-11-23 22:08:17 +0530

    Create Documentation
    Create readme.md file

--- db/tasksData.js
+++ db/tasksData.js
@@ -1,9 +1,21 @@
 const data = [
   {
+    id: 3,
+    title: "Prepare Monthly Report",
+    description: "Compile and analyze the data for the monthly report.",
+    flag: false,
+  },
+  {
     id: 1,
     title: "Complete Project Proposal",
     description:
       "Draft and finalize the project proposal for the upcoming client meeting.",
+    flag: false,
+  },
+  {
+    id: 5,
+    title: "Submit Expense Reports",
+    description: "Submit all expense reports by the end of the day.",
     flag: false,
   },
   {
@@ -12,23 +24,12 @@ const data = [
     description: "Pick up milk, eggs, and bread from the grocery store.",
     flag: true,
   },
-  {
-    id: 3,
-    title: "Prepare Monthly Report",
-    description: "Compile and analyze the data for the monthly report.",
-    flag: false,
-  },
+
   {
     id: 4,
     title: "Exercise",
     description: "Go for a 30-minute jog in the park.",
     flag: true,
-  },
-  {
-    id: 5,
-    title: "Submit Expense Reports",
-    description: "Submit all expense reports by the end of the day.",
-    flag: false,
   },
 ];
 
